73facdbd7806d902c9a62a275f94a442226e6cd0
nlucuab <Nyckolle.Lucuab@tlgcohort.com>
2023-03-15 15:16:00 -0700

    "Added Splash Screen ASCII art & initial clear screen method,AB#80"

---
+++ src/main/java/com/interstellarchef/Main.java
@@ -0,0 +1,10 @@
+package com.interstellarchef;
+
+import com.interstellarchef.view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        View viewInstance = new View();
+        viewInstance.printStartBanner();
+    }
+}

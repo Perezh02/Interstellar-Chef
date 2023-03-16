7314dddbd278b758cace9bc4f9465d0acfb2ae07
Kun-Li <kun.li@tlgcohort.com>
2023-03-16 16:10:02 -0400

    Add GameController class, AB#83

--- src/main/java/com/interstellarchef/Main.java
+++ src/main/java/com/interstellarchef/Main.java
@@ -1,11 +1,12 @@
 package com.interstellarchef;
 
+import com.interstellarchef.controller.GameController;
 import com.interstellarchef.view.View;
 
 public class Main {
     public static void main(String[] args) {
-        View viewInstance = new View();
-        viewInstance.printStartBanner();
-        viewInstance.printGameIntro();
+
+        GameController gameController= new GameController();
+        gameController.playGame();
     }
 }

72ee09dc3143bc91fd5b99823c47af3980847c2a
Changil Jeong <72395302+changiljeong@users.noreply.github.com>
2023-03-31 11:21:42 -0500

    Additional alignment of GUI game with game title, description, dialog popup, and listenHandler. AB#572, AB#573, AB#574, AB#575. (#6)
    * added in nested folder structure, for consistency, AB#80
    
    * Revert "Nested structure"
    
    * Implemented GUI game with game title, description, dialog popup, and listenHandler. AB#572, AB#573, AB#574, AB#575.
    
    * Additional alignment of GUI game with game title, description, dialog popup, and listenHandler. AB#572, AB#573, AB#574, AB#575.
    
    * Additional alignment of GUI game with game title, description, dialog popup, and listenHandler. AB#572, AB#573, AB#574, AB#575.
    
    ---------
    
    Co-authored-by: nlucuab <Nyckolle.Lucuab@tlgcohort.com>
    Co-authored-by: Sam Breece <sabreece@gmail.com>
    Co-authored-by: Nyckolle Lucuab <nlucuab18@gmail.com>

--- src/main/java/com/interstellarchef/GameFrame.java
+++ src/main/java/com/interstellarchef/GameFrame.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -20,21 +21,18 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.basic.BasicArrowButton;
 
 public class GameFrame extends JFrame{
 
   JLabel label1;
   JButton button;
-  JButton buttonSetting;
-  JButton buttonQuit;
+
 
   protected GameFrame() throws IOException {
     this.setTitle("Interstellar Chef");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setResizable(true);
+    this.setResizable(false);
     this.setSize(810,585);
     button = new JButton();
 
@@ -152,6 +150,20 @@ public class GameFrame extends JFrame{
 
 
   public void startMainGUI() throws IOException {
+
+    JButton buttonQuit = new JButton();
+    buttonQuit.setText("QUIT");
+    buttonQuit.setFocusable(false);
+    buttonQuit.setHorizontalAlignment(JButton.CENTER);
+    buttonQuit.setVerticalAlignment(JButton.CENTER);
+    buttonQuit.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    buttonQuit.setIconTextGap(5);
+    buttonQuit.setForeground(Color.WHITE);
+    buttonQuit.setBorder(BorderFactory.createEmptyBorder());
+    buttonQuit.setBackground(new Color(0xB45D5D));
+    buttonQuit.setBorder(BorderFactory.createEtchedBorder());
+    buttonQuit.setPreferredSize(new Dimension(100,30));
+
     JButton buttonHelp = new JButton();
     buttonHelp.setText("HELP");
     buttonHelp.setFocusable(false);
@@ -163,7 +175,7 @@ public class GameFrame extends JFrame{
     buttonHelp.setBorder(BorderFactory.createEmptyBorder());
     buttonHelp.setBackground(new Color(0x5B90E5));
     buttonHelp.setBorder(BorderFactory.createEtchedBorder());
-    buttonHelp.setPreferredSize(new Dimension(150,30));
+    buttonHelp.setPreferredSize(new Dimension(100,30));
 
     JButton buttonSetting = new JButton();
     buttonSetting.setText("SETTINGS");
@@ -176,46 +188,55 @@ public class GameFrame extends JFrame{
     buttonSetting.setBorder(BorderFactory.createEmptyBorder());
     buttonSetting.setBackground(new Color(0x5B90E5));
     buttonSetting.setBorder(BorderFactory.createEtchedBorder());
-    buttonSetting.setPreferredSize(new Dimension(150,30));
+    buttonSetting.setPreferredSize(new Dimension(100,30));
 
-    JButton buttonQuit = new JButton();
-    buttonQuit.setText("QUIT");
-    buttonQuit.setFocusable(false);
-    buttonQuit.setHorizontalAlignment(JButton.CENTER);
-    buttonQuit.setVerticalAlignment(JButton.CENTER);
-    buttonQuit.setFont(new Font("Comic Sans", Font.BOLD, 10));
-    buttonQuit.setIconTextGap(5);
-    buttonQuit.setForeground(Color.WHITE);
-    buttonQuit.setBorder(BorderFactory.createEmptyBorder());
-    buttonQuit.setBackground(new Color(0x5B90E5));
-    buttonQuit.setBorder(BorderFactory.createEtchedBorder());
-    buttonQuit.setPreferredSize(new Dimension(150,30));
+
 
 
 //     Button Action Listener calling HelpDialog PopUp
     buttonHelp.addActionListener(e -> new HelpDialog(this).helpPopUp());
+    buttonQuit.addActionListener(e -> System.exit(0));
+
+
 
     JPanel panelBg = new JPanel();
     JPanel panelTop = new JPanel();
     JPanel panelMain = new JPanel();
-    JPanel panelTextInput = new JPanel();
+    JPanel panelRight = new JPanel();
     JPanel panelRightTop = new JPanel();
+    JPanel panelRightMiddle = new JPanel();
     JPanel panelRightBottom = new JPanel();
+//    JPanel panelTextInput = new JPanel();
+
+
+    panelTop.setPreferredSize(new Dimension(500,300));
+    panelMain.setPreferredSize(new Dimension(500,300));
+    panelRightTop.setPreferredSize(new Dimension(500,300));
+    panelRightMiddle.setPreferredSize(new Dimension(500,300));
+    panelRightBottom.setPreferredSize(new Dimension(500,300));
+//    panelTextInput.setPreferredSize(new Dimension(500,300));
+
 
     panelBg.setBounds(0, 0, 800, 600);
 
     panelTop.setBounds(0, 0, 800, 50);
-    panelMain.setBounds(0, 50, 500, 450);
-    panelRightTop.setBounds(500, 50, 300, 300);
-    panelTextInput.setBounds(0, 500, 500, 50);
-    panelRightBottom.setBounds(500, 350, 300, 200);
+    panelMain.setBounds(0, 50, 500, 500);
+    panelRightTop.setBounds(500, 50, 300, 200);
+    panelRightMiddle.setBounds(500, 250, 300, 150);
+    panelRightBottom.setBounds(500, 400, 300, 150);
+//    panelTextInput.setBounds(0, 500, 500, 50);
 
     panelTop.setBorder(new EmptyBorder(5,0,0,0));
 
-    panelTextInput.setBackground(new Color(0x6225B0C7, true));
-    panelRightTop.setBackground(new Color(0x9A6F9A));
+//    panelTextInput.setBackground(new Color(0x6225B0C7, true));
+    panelTop.setBackground(new Color(0x657E9D));
+//    panelRightTop.setBackground(new Color(0x9A6F9A));
+    panelRightMiddle.setBackground(new Color(0xD6C5EE));
     panelRightBottom.setBackground(new Color(0xAD8EB688, true));
     panelMain.setBackground(new Color(0xC8DEFF));
+
+
+
 
     JLabel gameDescriptionLabel = new JLabel();
     gameDescriptionLabel.setText("<html>"
@@ -230,7 +251,7 @@ public class GameFrame extends JFrame{
         +"Finally: The Discovery Pod is the gateway to discovering neighboring planets!<br>"
         +"Beware of monsters, but interacting with them might yield something interesting.<br>"
         +"That's all for now, Chef! Good luck out there!<br><br>"
-        +"<h3>TYPE ANY COMMANDS OR PRESS BUTTONS TO START THE GAME.<h3><br>"
+        +"<h3>CLICK START BUTTON ON TOP OF THE SCREEN.<h3><br>"
 
         + "</html>");
 
@@ -239,73 +260,118 @@ public class GameFrame extends JFrame{
     gameDescriptionLabel.setForeground(new Color(0, 0, 0, 255));
     gameDescriptionLabel.setFont(new Font("Comic Sans", Font.PLAIN, 13));
 
+
+    JButton gameStartButton = new JButton();
+    gameStartButton.setText("START");
+    gameStartButton.setFocusable(false);
+    gameStartButton.setHorizontalAlignment(JButton.CENTER);
+    gameStartButton.setVerticalAlignment(JButton.CENTER);
+    gameStartButton.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    gameStartButton.setIconTextGap(5);
+    gameStartButton.setForeground(Color.WHITE);
+    gameStartButton.setBorder(BorderFactory.createEmptyBorder());
+    gameStartButton.setBackground(new Color(0xAD619B56));
+    gameStartButton.setBorder(BorderFactory.createEtchedBorder());
+    gameStartButton.setPreferredSize(new Dimension(100,30));
+
+    JLabel introImageLabel = new JLabel();
+
+//    try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream("spaceman2.jpg")) {
+//      byte[] buffer = inputStream.readAllBytes();
+//      Icon introImage = new ImageIcon(buffer);
+//      introImageLabel.setIcon(introImage);
+//    }
+
+    Icon introImage1 = new ImageIcon("src/main/resources/bg11.jpg");
+    introImageLabel.setIcon(introImage1);
+
+
+    gameStartButton.addActionListener(e -> {
+      gameStartButton.setEnabled(false);
+      panelMain.removeAll();
+      panelMain.repaint();
+      panelMain.add(introImageLabel);
+    });
+
+
+
     panelMain.add(gameDescriptionLabel);
 
 
-//    panelRightBottom.add(arrowEast);
-//    panelRightBottom.add(arrowNorth);
-//    panelRightBottom.add(arrowSouth);
+    panelRightBottom.setLayout(new BorderLayout());
+
+    JButton directionWest = new JButton();
+    directionWest.setText("WEST");
+    directionWest.setFocusable(false);
+    directionWest.setHorizontalAlignment(JButton.CENTER);
+    directionWest.setVerticalAlignment(JButton.CENTER);
+    directionWest.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    directionWest.setIconTextGap(5);
+    directionWest.setForeground(Color.WHITE);
+    directionWest.setBorder(BorderFactory.createEmptyBorder());
+    directionWest.setBackground(new Color(0x5B90E5));
+    directionWest.setBorder(BorderFactory.createEtchedBorder());
+    directionWest.setPreferredSize(new Dimension(50,15));
+
+    JButton directionEast = new JButton();
+    directionEast.setText("EAST");
+    directionEast.setFocusable(false);
+    directionEast.setHorizontalAlignment(JButton.CENTER);
+    directionEast.setVerticalAlignment(JButton.CENTER);
+    directionEast.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    directionEast.setIconTextGap(5);
+    directionEast.setForeground(Color.WHITE);
+    directionEast.setBorder(BorderFactory.createEmptyBorder());
+    directionEast.setBackground(new Color(0x5B90E5));
+    directionEast.setBorder(BorderFactory.createEtchedBorder());
+    directionEast.setPreferredSize(new Dimension(50,15));
+
+    JButton directionNorth = new JButton();
+    directionNorth.setText("NORTH");
+    directionNorth.setFocusable(false);
+    directionNorth.setHorizontalAlignment(JButton.CENTER);
+    directionNorth.setVerticalAlignment(JButton.CENTER);
+    directionNorth.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    directionNorth.setIconTextGap(5);
+    directionNorth.setForeground(Color.WHITE);
+    directionNorth.setBorder(BorderFactory.createEmptyBorder());
+    directionNorth.setBackground(new Color(0x5B90E5));
+    directionNorth.setBorder(BorderFactory.createEtchedBorder());
+    directionNorth.setPreferredSize(new Dimension(50,30));
+
+    JButton directionSouth = new JButton();
+    directionSouth.setText("SOUTH");
+    directionSouth.setFocusable(false);
+    directionSouth.setHorizontalAlignment(JButton.CENTER);
+    directionSouth.setVerticalAlignment(JButton.CENTER);
+    directionSouth.setFont(new Font("Comic Sans", Font.BOLD, 10));
+    directionSouth.setIconTextGap(5);
+    directionSouth.setForeground(Color.WHITE);
+    directionSouth.setBorder(BorderFactory.createEmptyBorder());
+    directionSouth.setBackground(new Color(0x5B90E5));
+    directionSouth.setBorder(BorderFactory.createEtchedBorder());
+    directionSouth.setPreferredSize(new Dimension(50,30));
+
+    JPanel buttonUpandDownPanel = new JPanel();
+    buttonUpandDownPanel.setLayout(new GridLayout(2,0));
+    JButton directionButtonUp = new JButton("UP");
+    JButton directionButtonDown = new JButton("DOWN");
+    directionButtonUp.setBorder(BorderFactory.createEtchedBorder());
+    directionButtonDown.setBorder(BorderFactory.createEtchedBorder());
+    directionButtonUp.setBackground(new Color(0x3EB62C));
+    directionButtonDown.setBackground(new Color(0x3EB52D));
+    directionButtonUp.setFocusable(false);
+    directionButtonDown.setFocusable(false);
+    buttonUpandDownPanel.add(directionButtonUp);
+    buttonUpandDownPanel.add(directionButtonDown);
 
 
-//    ImageIcon icon = new ImageIcon("src/main/resources/bg11.jpg");
-//    JButton panelButton1 = new JButton(icon);
-//    ImageIcon icon2 = new ImageIcon("src/main/resources/bg11.jpg");
-//    JButton panelButton2 = new JButton(icon2);
-//    ImageIcon icon3 = new ImageIcon("src/main/resources/bg11.jpg");
-//    JButton panelButton3 = new JButton(icon3);
-//    ImageIcon icon4 = new ImageIcon("src/main/resources/bg11.jpg");
-//    JButton panelButton4 = new JButton(icon4);
+    panelRightBottom.add(directionWest, BorderLayout.WEST);
+    panelRightBottom.add(directionEast, BorderLayout.EAST);
+    panelRightBottom.add(directionNorth, BorderLayout.NORTH);
+    panelRightBottom.add(directionSouth, BorderLayout.SOUTH);
+    panelRightBottom.add(buttonUpandDownPanel, BorderLayout.CENTER);
 
-//    panelRightBottom.setLayout(new BorderLayout());
-//    panelRightBottom.add(panelButton1, BorderLayout.EAST);
-//    panelRightBottom.add(panelButton2, BorderLayout.WEST);
-//    panelRightBottom.add(panelButton3, BorderLayout.SOUTH);
-//    panelRightBottom.add(panelButton4, BorderLayout.NORTH);
-
-    // Building direction buttons
-    panelRightBottom.setLayout(new GridLayout(3,3));
-    JButton directionButton1 = new JButton("1");
-    JButton directionButton2 = new JButton("NORTH");
-    JButton directionButton3 = new JButton("3");
-    JButton directionButton4 = new JButton("WEST");
-    JButton directionButton5 = new JButton("5");
-    JButton directionButton6 = new JButton("EAST");
-    JButton directionButton7 = new JButton("7");
-    JButton directionButton8 = new JButton("SOUTH");
-    JButton directionButton9 = new JButton("9");
-    directionButton1.setVisible(false);
-    directionButton3.setVisible(false);
-    directionButton5.setVisible(false);
-    directionButton7.setVisible(false);
-    directionButton9.setVisible(false);
-    directionButton2.setBorder(BorderFactory.createEtchedBorder());
-    directionButton4.setBorder(BorderFactory.createEtchedBorder());
-    directionButton6.setBorder(BorderFactory.createEtchedBorder());
-    directionButton8.setBorder(BorderFactory.createEtchedBorder());
-    directionButton2.setBackground(new Color(0x80ADFF));
-    directionButton4.setBackground(new Color(0x80ADFF));
-    directionButton6.setBackground(new Color(0x80ADFF));
-    directionButton8.setBackground(new Color(0x80ADFF));
-    directionButton2.setFocusable(false);
-    directionButton4.setFocusable(false);
-    directionButton6.setFocusable(false);
-    directionButton8.setFocusable(false);
-
-    panelRightBottom.add(directionButton1);
-    panelRightBottom.add(directionButton2);
-    panelRightBottom.add(directionButton3);
-    panelRightBottom.add(directionButton4);
-    panelRightBottom.add(directionButton5);
-    panelRightBottom.add(directionButton6);
-    panelRightBottom.add(directionButton7);
-    panelRightBottom.add(directionButton8);
-    panelRightBottom.add(directionButton9);
-
-
-
-    JLabel label2 = new JLabel();
-    label2.setHorizontalAlignment(JLabel.CENTER); // set text LEFT, CENTER, RIGHT of imageicon
-    label2.setVerticalAlignment(JLabel.TOP); // set text TOP, CENTER, BOTTOM of imageicon
 
     JTextArea inventoryTextArea = new JTextArea(10,27);
     inventoryTextArea.append("- Current Location: DATA FROM JSON\n" );
@@ -317,10 +383,13 @@ public class GameFrame extends JFrame{
 
     inventoryTextArea.setLineWrap(true);
 
-    panelRightTop.add(inventoryTextArea);
+//    panelRightTop.add(inventoryTextArea);
+
+    panelTop.add(gameStartButton);
     panelTop.add(buttonHelp);
     panelTop.add(buttonSetting);
     panelTop.add(buttonQuit);
+
 
 
 
@@ -331,8 +400,27 @@ public class GameFrame extends JFrame{
     layeredPane.add(panelTop,Integer.valueOf(3));
     layeredPane.add(panelMain,Integer.valueOf(4));
     layeredPane.add(panelRightTop,Integer.valueOf(1));
+    layeredPane.add(panelRightMiddle,Integer.valueOf(7));
     layeredPane.add(panelRightBottom,Integer.valueOf(5));
-    layeredPane.add(panelTextInput,Integer.valueOf(2));
+//    layeredPane.add(panelTextInput,Integer.valueOf(2));
+
+//
+//    panelRight.setLayout(new BorderLayout());
+//    panelRight.add(panelRightTop, BorderLayout.NORTH);
+//    panelRight.add(panelRightMiddle, BorderLayout.CENTER);
+//    panelRight.add(panelRightBottom, BorderLayout.SOUTH);
+//
+
+
+//
+//
+//    this.setLayout(new BorderLayout());
+//    this.add(panelTop, BorderLayout.NORTH);
+//    this.add(panelMain, BorderLayout.CENTER);
+//    this.add(panelRight, BorderLayout.EAST);
+
+
+
 
     this.add(layeredPane);
     this.setVisible(true);
--- src/main/java/com/interstellarchef/Main.java
+++ src/main/java/com/interstellarchef/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         new GameFrame().introGUI();
 //        new GameController();
-//        new Test().test();
-
     }
 }
+
